c313668dd2908ae4a55c7abfa49f8efd09452c67
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-12-04 12:09:15 +0400

    updated CoordArray

--- CoordArray.js
+++ CoordArray.js
@@ -10,26 +10,23 @@ return declare(null, {
 	},
 
 	push: function(feature, point) {
-		var coords = feature.getCoords(),
-			graphic = feature.baseShapes[0],
+		var graphic = feature.baseShapes[0],
 			geometry = graphic.geometry
 		;
-		point = feature.map.getCoords(point);
 		if (feature.getCoordsType() == "LineString") {
-			coords.push(point);
+			var coords = feature.getCoords();
 			geometry.insertPoint(0, coords.length-1, esri.geometry.geographicToWebMercator( new esri.geometry.Point(point[0], point[1], {wkid: 4326}) ));
 			graphic.setGeometry(geometry);
 		}
 	},
 
 	set: function(feature, index, point) {
-		var coords = feature.getCoords(),
-			geometry = feature.baseShapes[0].geometry
+		var graphic = feature.baseShapes[0],
+			geometry = graphic.geometry
 		;
-		point = feature.map.getCoords(point);
 		if (feature.getCoordsType() == "LineString") {
-			coords[index] = point;
-			geometry.setPoint(0, index, point)
+			geometry.setPoint(0, index, point);
+			graphic.setGeometry(geometry);
 		}
 	},
 
